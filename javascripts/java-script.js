157cd5c3c58589490b79673bea1c4d4c426d35e0
nonitut <nona.blogg@gmail.com>
2022-02-06 20:27:08 +0300

    6/02 add content

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,1 +1,22 @@
 //write here your js
+$(document).ready(function(){
+  $('.slide__thumbnails .thumb').click(function(){
+    var imgThumb = $(this).attr('src');
+    // attr - возвращает undefined для тех атрибутов, значение которых не было задано
+    var oldImg = $('.full_img');
+    var imgFull = $('<img class="full_img" src="' + imgThumb +'">');
+    imgFull.hide(); // hide - позволяет скрывать выбранные элементы
+    $('.full').prepend(imgFull); //prepend - Добавляет контент внутрь выбранных элементов
+    imgFull.show(1);
+    oldImg.hide(1,function(){
+      $(this).remove();
+    });
+  });
+});
+$('thumb:first').click();
+
+let y;
+$(window).on('scroll', function() {
+	y = $(window).scrollTop();
+	$('.psection4').css('right',5 + parseInt(y-1250) + '%');
+});
